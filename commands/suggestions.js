a1da6073e7bd9e3538de9fa32471d82e97381003
BurzaKrual <darthakre@protonmail.com>
2021-04-13 21:42:47 +0300

    Update suggestions.js

--- commands/suggestions.js
+++ commands/suggestions.js
@@ -1,15 +1,14 @@
-const { MessageEmbed } = require('discord.js');
 module.exports = {
     name: 'suggestions',
     aliases: ['suggest', 'suggestion'],
     permissions: [],
     description: 'creates a suggestion!',
-    execute(message, args,client, Discord){
+    execute(Discord,message, args,client){
         const channel = message.guild.channels.cache.find(c => c.name === 'suggestions');
         if(!channel) return message.channel.send('suggestions channel does not exist!');
 
         let messageArgs = args.join(' ');
-        const embed = new MessageEmbed()
+        const embed = new Discord.MessageEmbed()
         .setColor('FADF2E')
         .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
         .setDescription(messageArgs);
