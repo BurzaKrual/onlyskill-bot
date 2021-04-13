edd4107fbedc39bbe06e19f66dc499ad29b696e6
BurzaKrual <darthakre@protonmail.com>
2021-04-13 21:35:23 +0300

    Update suggestions.js

--- commands/suggestions.js
+++ commands/suggestions.js
@@ -5,7 +5,9 @@ module.exports = {
     permissions: [],
     description: 'creates a suggestion!',
     execute(message, args,client, Discord){
-        const channel = '826548663328702482';
+        const channel = message.guild.channels.cache.find(c => c.name === 'suggestions');
+        if(!channel) return message.channel.send('suggestions channel does not exist!');
+
         let messageArgs = args.join(' ');
         const embed = new MessageEmbed()
         .setColor('FADF2E')
