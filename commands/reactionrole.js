6407c322a7e2aa0a8a82b040ac67c3ba05cda328
BurzaKrual <darthakre@protonmail.com>
2021-04-13 10:57:24 +0300

    Update reactionrole.js

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -2,58 +2,60 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
     name: 'reactionrole',
     destcription:'Sets up a Reaction Role Message',
-    execute(message, args, Discord, client){
-        const channel = '831431041684078643';
-        const Test1 = message.guild.roles.cache.find(role => role.name === "TEST1");
-        const Test2 = message.guild.roles.cache.find(role => role.name === "TEST2");
- 
-        const Test1Emoji = '⚫';
-        const Test2Emoji = '🔴';
- 
+    async run (bot, message, args){
+        const channelID = message.mentions.channels.first();
+        if(!channelID) return message.reply("Please specify a valid channel you want the embed to be sent in!\n `Example: .reactionrole #<channel> <Your Description>`")
+
+        const desc = args.slice(1).join(" ")
+        if(!desc) return message.reply("Please add a valid description!\n `Example: .reactionrole #<channel> <Your Description>`")
+
+
+        const Role1 = message.guild.roles.cache.find(role => role.name === "TEST1")
+        const Role2 = message.guild.roles.cache.find(role => role.name === "TEST2")
+
+        const emoji1 = '🔴';
+        const emoji2 = '⚫';
+
+
         let embed = new MessageEmbed()
-            .setColor('#e42643')
-            .setTitle('TEST!')
-            .setDescription('Test!\n\n'
-                + `${Test1Emoji} for Test1 role\n`
-                + `${Test2Emoji} for Test2 role`);
- 
-        let messageEmbed = message.channel.send(embed);
-        messageEmbed.react(Test1Emoji);
-        messageEmbed.react(Test2Emoji);
- 
-        client.on('messageReactionAdd', async (reaction, user) => {
+        .setColor("RANDOM")
+        .setTitle("Title")
+        .setDescription(desc)
+
+        let msgembed = await channelID.send(embed)
+        await msgembed.react(emoji1)
+        await msgembed.react(emoji2)
+
+        bot.on('messageReactionAdd', async (reaction, user) => {
             if (reaction.message.partial) await reaction.message.fetch();
             if (reaction.partial) await reaction.fetch();
             if (user.bot) return;
             if (!reaction.message.guild) return;
- 
-            if (reaction.message.channel.id == channel) {
-                if (reaction.emoji.name === Test1Emoji) {
-                    await reaction.message.guild.members.cache.get(user.id).roles.add(Test1);
+
+            if (reaction.message.channel.id == channelID) {
+                if (reaction.emoji.name === emoji1) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(Role1)
                 }
-                if (reaction.emoji.name === Test2Emoji) {
-                    await reaction.message.guild.members.cache.get(user.id).roles.add(Test2);
+                if (reaction.emoji.name === emoji2) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(Role2)
                 }
             } else {
                 return;
             }
- 
         });
- 
-        client.on('messageReactionRemove', async (reaction, user) => {
- 
+
+        bot.on('messageReactionRemove', async (reaction, user) => {
             if (reaction.message.partial) await reaction.message.fetch();
             if (reaction.partial) await reaction.fetch();
             if (user.bot) return;
             if (!reaction.message.guild) return;
- 
- 
-            if (reaction.message.channel.id == channel) {
-                if (reaction.emoji.name === Test1Emoji) {
-                    await reaction.message.guild.members.cache.get(user.id).roles.remove(Test1);
+
+            if (reaction.message.channel.id == channelID) {
+                if (reaction.emoji.name === emoji1) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(Role1)
                 }
-                if (reaction.emoji.name === Test2Emoji) {
-                    await reaction.message.guild.members.cache.get(user.id).roles.remove(Test2);
+                if (reaction.emoji.name === emoji2) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(Role2)
                 }
             } else {
                 return;
